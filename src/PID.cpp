9e035d29cad2892c4883a08353c1075841f2f56b
Neeraj Phadnis <nvp.f1crazer@gmail.com>
2018-10-29 01:54:47 -0400

    Upload files

---
+++ src/PID.cpp
@@ -0,0 +1,35 @@
+#include "PID.h"
+
+using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp, double Ki, double Kd){
+	this->Kp = Kp;
+	this->Ki = Ki;
+	this->Kd = Kd;
+	p_error = 0;
+	i_error = 0;
+	d_error = 0;
+	is_initialized = true;
+}
+
+void PID::UpdateError(double cte) {
+	d_error = cte-p_error;
+	p_error = cte;
+	i_error += cte;	
+}
+
+double PID::TotalError() {
+	double total_error = -Kp*p_error - Ki*i_error - Kd*d_error;
+	if (total_error > 1.0) {total_error = 1.0;}
+	else if (total_error < -1.0) {total_error = -1.0;}
+	return total_error;
+}
+
